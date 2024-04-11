e7613a6c400794a8594db742cb59cf0c8894ecfd
MDA-Han-Student <117663171+MDA-Han-Student@users.noreply.github.com>
2024-04-11 14:53:06 +0200

    init

---
+++ chart-lib-research/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Nav />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ chart-lib-research/src/components/Nav.js
@@ -0,0 +1,11 @@
+function Nav() {
+    return (
+        <ul>
+          <li>Chart 1</li>
+          <li>Chart 1</li>
+          <li>Chart 1</li>
+          <li>Chart 1</li>
+          <li>Chart 1</li>
+        </ul>
+    );
+}
