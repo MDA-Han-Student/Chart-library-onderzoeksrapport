c50550c1296b2ed879bb0966684270e8ca1fdf1b
Jelle Schaafsma <55688800+Mino03947@users.noreply.github.com>
2024-04-15 11:52:12 +0200

    ??? waarom wil nivo niet werken?

--- chart-lib-research/src/components/charts/Nivo.js
+++ chart-lib-research/src/components/charts/Nivo.js
@@ -1,7 +1,46 @@
 import React from "react";
 import "nivo";
 import {ResponsiveLine} from "nivo";
+import {ResponsiveScatterPlot} from "@nivo/scatterplot";
 
+const data = [
+    {
+        id: 'Oil Temp',
+        "color": "hsl(32, 70%, 50%)",
+        data: [
+        {
+        x: '2021-01-01',
+        y: 0,
+        },
+        {
+        x: '2021-01-02',
+        y: 1,
+        },
+        {
+        x: '2021-01-03',
+        y: 2,
+        },
+        ]
+    },
+    {
+        id: 'Oil Pressure',
+        "color": "hsl(159,70%,50%)",
+        data: [
+        {
+        x: '2021-01-01',
+        y: 1,
+        },
+        {
+        x: '2021-01-02',
+        y: 2,
+        },
+        {
+        x: '2021-01-03',
+        y: 3,
+        },
+        ]
+    }
+];
 
 
 const MyResponsiveLine = ({ data /* see data tab */ }) => (
@@ -73,21 +112,223 @@ const MyResponsiveLine = ({ data /* see data tab */ }) => (
     />
 )
 
+// const MyResponsiveRealTimeChartComponent = ({ data /* see data tab */ }) => (
+//     <RealTimeChartComponent
+//         data={data}
+//         margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+//         xScale={{ type: 'point' }}
+//         yScale={{
+//             type: 'linear',
+//             min: 'auto',
+//             max: 'auto',
+//             stacked: true,
+//             reverse: false
+//         }}
+//         yFormat=" >-.2f"
+//         axisTop={null}
+//         axisRight={[1,2,3,4,5,6,7,8]}
+//         axisBottom={{
+//             tickSize: 5,
+//             tickPadding: 5,
+//             tickRotation: 0,
+//             legend: 'transportation',
+//             legendOffset: 36,
+//             legendPosition: 'middle',
+//             truncateTickAt: 0
+//         }}
+//         axisLeft={{
+//             tickSize: 5,
+//             tickPadding: 5,
+//             tickRotation: 0,
+//             legend: 'count',
+//             legendOffset: -40,
+//             legendPosition: 'middle',
+//             truncateTickAt: 0
+//         }}
+//         pointSize={10}
+//         pointColor={{ theme: 'background' }}
+//         pointBorderWidth={2}
+//         pointBorderColor={{ from: 'serieColor' }}
+//         pointLabelYOffset={-12}
+//         enableTouchCrosshair={true}
+//         useMesh={true}
+//         legends={[
+//             {
+//                 anchor: 'bottom-right',
+//                 direction: 'column',
+//                 justify: false,
+//                 translateX: 100,
+//                 translateY: 0,
+//                 itemsSpacing: 0,
+//                 itemDirection: 'left-to-right',
+//                 itemWidth: 80,
+//                 itemHeight: 20,
+//                 itemOpacity: 0.75,
+//                 symbolSize: 12,
+//                 symbolShape: 'circle',
+//                 symbolBorderColor: 'rgba(0, 0, 0, .5)',
+//                 effects: [
+//                     {
+//                         on: 'hover',
+//                         style: {
+//                             itemBackground: 'rgba(0, 0, 0, .03)',
+//                             itemOpacity: 1
+//                         }
+//                     }
+//                 ]
+//             }
+//         ]}
+//     />
+// )
+//
+// const MyResponsiveScatterPlot = ({ data /* see data tab */ }) => (
+//     <ResponsiveScatterPlot
+//         data={data}
+//         margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+//         xScale={{ type: 'point' }}
+//         yScale={{
+//             type: 'linear',
+//             min: 'auto',
+//             max: 'auto',
+//             stacked: true,
+//             reverse: false
+//         }}
+//         yFormat=" >-.2f"
+//         axisTop={null}
+//         axisRight={null}
+//         axisBottom={{
+//             tickSize: 5,
+//             tickPadding: 5,
+//             tickRotation: 0,
+//             legend: 'transportation',
+//             legendOffset: 36,
+//             legendPosition: 'middle',
+//             truncateTickAt: 0
+//         }}
+//         axisLeft={{
+//             tickSize: 5,
+//             tickPadding: 5,
+//             tickRotation: 0,
+//             legend: 'count',
+//             legendOffset: -40,
+//             legendPosition: 'middle',
+//             truncateTickAt: 0
+//         }}
+//         pointSize={10}
+//         pointColor={{ theme: 'background' }}
+//         pointBorderWidth={2}
+//         pointBorderColor={{ from: 'serieColor' }}
+//         pointLabelYOffset={-12}
+//         enableTouchCrosshair={true}
+//         useMesh={true}
+//         legends={[
+//             {
+//                 anchor: 'bottom-right',
+//                 direction: 'column',
+//                 justify: false,
+//                 translateX: 100,
+//                 translateY: 0,
+//                 itemsSpacing: 0,
+//                 itemDirection: 'left-to-right',
+//                 itemWidth: 80,
+//                 itemHeight: 20,
+//                 itemOpacity: 0.75,
+//                 symbolSize: 12,
+//                 symbolShape: 'circle',
+//                 symbolBorderColor: 'rgba(0, 0, 0, .5)',
+//                 effects: [
+//                     {
+//                         on: 'hover',
+//                         style: {
+//                             itemBackground: 'rgba(0, 0, 0, .03)',
+//                             itemOpacity: 1
+//                         }
+//                     }
+//                 ]
+//             }
+//         ]}
+//     />
+// )
+//
+// const MyResponsiveDotPlot = ({ data /* see data tab */ }) => (
+//     <ResponsiveScatterPlot
+//         data={data}
+//         margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+//         xScale={{ type: 'point' }}
+//         yScale={{
+//             type: 'linear',
+//             min: 'auto',
+//             max: 'auto',
+//             stacked: true,
+//             reverse: false
+//         }}
+//         yFormat=" >-.2f"
+//         axisTop={null}
+//         axisRight={null}
+//         axisBottom={{
+//             tickSize: 5,
+//             tickPadding: 5,
+//             tickRotation: 0,
+//             legend: 'transportation',
+//             legendOffset: 36,
+//             legendPosition: 'middle',
+//             truncateTickAt: 0
+//         }}
+//         axisLeft={{
+//             tickSize: 5,
+//             tickPadding: 5,
+//             tickRotation: 0,
+//             legend: 'count',
+//             legendOffset: -40,
+//             legendPosition: 'middle',
+//             truncateTickAt: 0
+//         }}
+//         pointSize={10}
+//         pointColor={{ theme: 'background' }}
+//         pointBorderWidth={2}
+//         pointBorderColor={{ from: 'serieColor' }}
+//         pointLabelYOffset={-12}
+//         enableTouchCrosshair={true}
+//         useMesh={true}
+//         legends={[
+//             {
+//                 anchor: 'bottom-right',
+//                 direction: 'column',
+//                 justify: false,
+//                 translateX: 100,
+//                 translateY: 0,
+//                 itemsSpacing: 0,
+//                 itemDirection: 'left-to-right',
+//                 itemWidth: 80,
+//                 itemHeight: 20,
+//                 itemOpacity: 0.75,
+//                 symbolSize: 12,
+//                 symbolShape: 'circle',
+//                 symbolBorderColor: 'rgba(0, 0, 0, .5)',
+//                 effects: [
+//                     {
+//                         on: 'hover',
+//                         style: {
+//                             itemBackground: 'rgba(0, 0, 0, .03)',
+//                             itemOpacity: 1
+//                         }
+//                     }
+//                 ]
+//             }
+//         ]}
+//     />
+// )
+
 const Nivo = () => {
     console.log("Nivo");
-    // return (
-    //     // <MyResponsiveLine data={} />
-    // )
+    return (
+        <>
+        <MyResponsiveLine data={data} />
+        {/*<MyResponsiveScatterPlot data={data} />*/}
+        {/*<MyResponsiveDotPlot data={data} />*/}
+        {/*<MyResponsiveRealTimeChartComponent data={data} />*/}
+        </>
+    )
 }
-
-
-const data = [
-    {name: 'Page A', uv: 400, pv: 2400, amt: 900},
-    {name: 'Page B', uv: 450, pv: 2200, amt: 1200},
-    {name: 'Page C', uv: 500, pv: 2250, amt: 1130},
-    {name: 'Page D', uv: 525, pv: 1900, amt: 850},
-    {name: 'Page E', uv: 585, pv: 1770, amt: 1050},
-    {name: 'Page F', uv: 635, pv: 2300, amt: 1250},
-];
 
 export default Nivo;
